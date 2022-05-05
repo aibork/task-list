e18f242037ef1984c45d6bb75dc2c8dfaf2c1320
AxelB <axelbork@outlook.com>
2022-05-05 10:37:07 -0300

    Lista Tareas

---
+++ src/app/components/mock-task.ts
@@ -0,0 +1,28 @@
+import { Task } from "./Task"
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Terminar promer modulo de Angular",
+        day: 'Agosto 5 a las 12:00',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: "Hacer las compras",
+        day: 'Agosto 6 a las 12:00',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: "Investigar sobre Boostrap",
+        day: 'Agosto 7 a las 12:00',
+        reminder: true,
+    },
+    {
+        id: 4,
+        text: "Ir a pescar",
+        day: 'Agosto 8 a las 12:00',
+        reminder: true,
+    },
+
+]
