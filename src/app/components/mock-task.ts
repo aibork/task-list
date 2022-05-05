c0120dab8f33382bf2d56d4c54d8c32e938cfa24
AxelB <axelbork@outlook.com>
2022-05-05 13:06:30 -0300

    Task List integrada con task

--- src/app/components/mock-task.ts
+++ src/app/components/mock-task.ts
@@ -4,25 +4,25 @@ export const TASKS: Task[] = [
     {
         id: 1,
         text: "Terminar promer modulo de Angular",
-        day: 'Agosto 5 a las 12:00',
+        day: "Agosto 5 a las 12:00",
         reminder: true,
     },
     {
         id: 2,
         text: "Hacer las compras",
-        day: 'Agosto 6 a las 12:00',
+        day: "Agosto 6 a las 12:00",
         reminder: true,
     },
     {
         id: 3,
         text: "Investigar sobre Boostrap",
-        day: 'Agosto 7 a las 12:00',
+        day: "Agosto 7 a las 12:00",
         reminder: true,
     },
     {
         id: 4,
         text: "Ir a pescar",
-        day: 'Agosto 8 a las 12:00',
+        day: "Agosto 8 a las 12:00",
         reminder: true,
     },
 
