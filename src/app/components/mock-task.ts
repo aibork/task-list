1f2d4056886ded601bded64fa1db7b8903e895a0
AxelB <axelbork@outlook.com>
2022-05-07 13:23:04 -0300

    Pack Services

--- src/app/components/mock-task.ts
+++ src/app/components/mock-task.ts
@@ -8,13 +8,19 @@ export const TASKS: Task[] = [
         reminder: true,
     },
     {
-        id: 2,
-        text: "Hacer las compras",
+        id: 3,
+        text: "Comprar Carnada",
         day: "Sábado 7 de Mayo a las 10:00 hs",
         reminder: true,
     },
     {
         id: 3,
+        text: "Hacer las compras",
+        day: "Sábado 7 de Mayo a las 14:00 hs",
+        reminder: true,
+    },
+    {
+        id: 4,
         text: "Ir a pescar",
         day: "Domigo 8 de Mayo a las 07:00 hs",
         reminder: true,
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Task } from '../Task'
-import { TASKS } from '../mock-task'
+import { TaskService } from 'src/app/service/task.service';
+import { Task } from '../Task';
 
 
 @Component({
@@ -9,11 +9,16 @@ import { TASKS } from '../mock-task'
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
+  tasks: Task[] = [];
   
-  constructor() { }
+  constructor(
+    private taskServices: TaskService
+  ) { }
 
   ngOnInit(): void {
+    //Like Promise
+    this.taskServices.getTask().subscribe((tasks) =>
+      this.tasks = tasks);
   }
 
 }
