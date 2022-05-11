a06f723521ad505a8d5609e112852bfd72af2d61
AxelB <axelbork@outlook.com>
2022-05-10 21:40:22 -0300

    Toggle en la base de datos

--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHandler} from '@angular/common/http'
+import {HttpClient, HttpHandler, HttpHeaders} from '@angular/common/http'
 import { Observable, of } from 'rxjs';
 import { Task } from '../components/Task';
 import { TASKS } from '../components/mock-task';
+
+const httpsOption = {
+  headers: new HttpHeaders({
+    'Content-Type':'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-  private apiUrl = ' http://localhost:5001/tasks'
+  private apiUrl = ' http://localhost:5000/tasks'
 
   constructor(
     private http: HttpClient
@@ -22,5 +28,10 @@ export class TaskService {
   deleteTask(task:Task): Observable <Task>{
     const url = `${this.apiUrl}/${task.id}`
     return this.http.delete<Task>(url)
+  }
+
+  updateTaskReminder(task: Task):Observable <Task>{
+    const url = `${this.apiUrl}/${task.id}`
+    return this.http.put<Task>(url,task,httpsOption)
   }
 }
